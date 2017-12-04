1db922edb5284a521832e8ecc68976036d4dc58a
hiroyukiasada <poney10060626@gmail.com>
2017-12-04 01:02:05 +0000

    管理機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,60 @@
 class TasksController < ApplicationController
 
 def index
+  @tasks = Task.all
 end
 
 def show 
+  set_task
+
 end
 
 def new
+  @task = Task.new
+end
+
+def create
+  @task = Task.new(task_params)
+  if @task.save
+    flash[:success] = '正常に投稿されました'
+    redirect_to @task
+  else
+    flash.now[:danger] = '正常に投稿されませんでした'
+    render :new
+  end
 end
 
 def edit
+  set_task
+  
 end
 
+def update
+  set_task
+  
+  if @task.update(task_params)
+    flash[:success] = '正常に投稿されました'
+    redirect_to @task
+  else
+    flash.now[:danger] = '正常に投稿されませんでした'
+    render :edit
+  end
+end
 
+def destroy
+    set_task
+    @task.destroy
 
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to tasks_url
+end
+
+private
+def set_task
+    @task = Task.find(params[:id])
+end
+
+def task_params
+     params.require(:task).permit(:content)
+end
 end
